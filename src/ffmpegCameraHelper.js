107bd8f526b0fc32021f0dc671e8d7360fab2b6f
Vladimir <intfloatbool@gmail.com>
2019-08-28 19:33:18 +0300

    cams

--- src/ffmpegCameraHelper.js
+++ src/ffmpegCameraHelper.js
@@ -1,7 +1,22 @@
+const nodeWebCam = require('node-webcam');
+const dshow = require('directcam');
+
+//TEST
+getNameOfCameras();
+
+function getNameOfCameras() {
+    let cameras = [];
+    dshow.cameras(null, function (err, data) {
+        const camsJson = JSON.stringify(data);
+        console.log(`CAMERAS: \n ${camsJson}`);
+    });
+}
+
 function parseDataForCameras(dataToParse) {
 
 }
 
 module.exports = {
-    parseDataForCameras
+    parseDataForCameras,
+    getNameOfCameras
 }
