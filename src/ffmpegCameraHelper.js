c0f406f3d073fa85dfc14b447978c3e85d549a96
Vladimir <intfloatbool@gmail.com>
2019-08-29 17:12:28 +0300

    Camera names parser

--- src/ffmpegCameraHelper.js
+++ src/ffmpegCameraHelper.js
@@ -4,20 +4,53 @@ const getCameraNamesAsync = async (ffmpegPath) => {
     let cmdData;
     await getProcessData(ffmpegPath).then(
         (data) => {
-            console.log("Try to get data from cmd...");
             cmdData = data;
         });
-    console.log("DATA GETTED: " + cmdData)
     const parsedCameraNames = await getParsedCameraNames(cmdData);  
     
     await parsedCameraNames;
 }
 
 const getParsedCameraNames = async (data) => {
-    const lines = data.split('\n');
-    console.log("LINES: " + lines.length);
-    return lines;
+
+    let camNames = [];
+
+    const dataLines = data.split('\n');
+    console.log("LINES LENGTH: " + dataLines.length);
+    const videoKeyWord = "DirectShow video devices";
+    const audioKeyWord = "DirectShow audio devices";
+    const alternativeNameKey = "Alternative name";
+    let isVideoDevicesDesribed = false;
+
+    for(let i = 0; i < dataLines.length; i++) {
+        let line = dataLines[i];
+        if (line.includes(videoKeyWord) && isVideoDevicesDesribed == false) {
+            isVideoDevicesDesribed = true;
+            continue;
+        }
+
+        if(line.includes(audioKeyWord))
+            break;
+        if(isVideoDevicesDesribed && line.includes(alternativeNameKey))
+            continue;
+        if(isVideoDevicesDesribed) {
+            const camName = getCamName(line);
+            camNames.push({name: camName});
+        }
+    }
+    console.log(`CAM NAMES: \n ${JSON.stringify(camNames)}`);
+    return camNames;
 };
+
+const getCamName = (camLine) => {
+    let nameOfCamera = '';
+    let camNameIndex = camLine.indexOf("\"");
+    nameOfCamera = camLine.substring(camNameIndex)
+        .replace("\"", '')
+            .replace("\"", '')
+                .replace("\r", '');
+    return nameOfCamera;
+}
 
 const getProcessData = (ffmpegPath) => {
     return new Promise((resolve, reject) => {
