b7e1b6787fc553c8ba61b2acdc46036bc7e3d4df
Vladimir <intfloatbool@gmail.com>
2019-09-10 16:40:03 +0300

    Stream chunks folder creation

--- front/streamWatchPage/index.js
+++ front/streamWatchPage/index.js
@@ -7,6 +7,6 @@ $(document).ready(function() {
 
 ipc.on('streamerDataGetted', (event, args) => {
     const streamData = args;
-    //alert("STREAM DATA!\n" + JSON.stringify(args));
+    alert("STREAM DATA!\n" + JSON.stringify(streamData));
 });
 
--- src/pages/streamWatchPage.js
+++ src/pages/streamWatchPage.js
@@ -1,15 +1,45 @@
 const Room = require('ipfs-pubsub-room');
+const dataConverter = require('../helpers/dataConverters.js');
+const pathModule = require('path');
+const appRootPath = require('app-root-path');
+const fs = require('fs');
+
+const STREAMERS_DATA_PATH = pathModule.join(appRootPath.toString(), 'user','userData','streamers');
 
 class StreamWatchPage {
     constructor(ipfs, ipc, win, streamerInfo){
         this.ipfs = ipfs;
         this.ipc = ipc;
         this.win = win;
-        this.streamerInfo = streamerInfo;
-        
-        win.webContents.send('streamerDataGetted', this.streamerInfo);
-        
+        this.streamerInfo = streamerInfo;    
+        const streamWatchPageObj = this;
+             
         this.subscribeToStreamerRoom(this.streamerInfo);      
+        this.initializeStreamerPath(this.streamerInfo).then((path) => {
+            //DO something when path exists
+            streamWatchPageObj.win.webContents.send('streamerDataGetted', this.streamerInfo);
+            streamWatchPageObj.createTranslationFolder(path);
+        }).catch((err) => {
+            throw err;
+        })
+    }
+
+    initializeStreamerPath(streamerInfo) {
+        const streamWatchObj = this;
+        const streamerHash = streamerInfo.hashOfStreamer;
+        this.currentStreamerPath = pathModule.join(STREAMERS_DATA_PATH, streamerHash);
+        return new Promise((resolve, rejected) => {
+            if(fs.existsSync(streamWatchObj.currentStreamerPath)) {
+                resolve(streamWatchObj.currentStreamerPath);
+            } else {
+                rejected(new Error("Path not exists: ") + streamWatchObj.currentStreamerPath);
+            }
+        });
+    }
+
+    createTranslationFolder(streamerPath) {
+        this.streamerVideoFolder = pathModule.join(streamerPath, 'streamChunks');
+        fs.mkdirSync(this.streamerVideoFolder);
     }
 
     subscribeToStreamerRoom(streamerInfo) {
