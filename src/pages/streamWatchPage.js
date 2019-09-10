cc955aed0af7be166dd63821713cb9e7069f9e8c
Vladimir <intfloatbool@gmail.com>
2019-09-10 17:10:29 +0300

    streamChunks creation

--- src/pages/streamWatchPage.js
+++ src/pages/streamWatchPage.js
@@ -3,6 +3,7 @@ const dataConverter = require('../helpers/dataConverters.js');
 const pathModule = require('path');
 const appRootPath = require('app-root-path');
 const fs = require('fs');
+const localServer = require('../localServer/localServer.js');
 
 const STREAMERS_DATA_PATH = pathModule.join(appRootPath.toString(), 'user','userData','streamers');
 
@@ -11,9 +12,10 @@ class StreamWatchPage {
         this.ipfs = ipfs;
         this.ipc = ipc;
         this.win = win;
-        this.streamerInfo = streamerInfo;    
-        const streamWatchPageObj = this;
-             
+        this.streamerInfo = streamerInfo;
+        this.lastBlockIndex = 0;
+        
+        const streamWatchPageObj = this;           
         this.subscribeToStreamerRoom(this.streamerInfo);      
         this.initializeStreamerPath(this.streamerInfo).then((path) => {
             //DO something when path exists
@@ -43,6 +45,7 @@ class StreamWatchPage {
     }
 
     subscribeToStreamerRoom(streamerInfo) {
+        const streamWatchPageObj = this;
         const streamHash = streamerInfo.hashOfStreamer;
         console.log("Subscribe to streamer room name: " + streamHash);
         this.streamerRoom = Room(this.ipfs, streamHash);
@@ -53,7 +56,81 @@ class StreamWatchPage {
         this.streamerRoom.on('message', (msg) => {
             const messageStr = msg.data.toString();
             console.log("Getted message from streamer: " + messageStr);
+            streamWatchPageObj.onStreamDataGetted(messageStr);
         });
+    }
+
+    async onStreamDataGetted(streamData) {
+        const streamWatchPageObj = this;
+        try {
+            const streamBlock = dataConverter.convertBase64DataToObject(streamData);
+            const chunkHash = streamBlock.VIDEO_CHUNK_HASH;
+            const extInf = streamBlock.EXTINF;
+            const lastChunkData = await new Promise((resolve, rejected) => {
+                streamWatchPageObj.ipfs.get(chunkHash, (err, files) => {
+                    if(err) {
+                        rejected(err);
+                    }
+                    const chunkName = 'master' + streamWatchPageObj.lastBlockIndex + '.ts';
+                    const chunkPath = pathModule.join(streamWatchPageObj.currentStreamerPath, chunkName);
+                    const file = files[0];
+                    const buffer = file.content;
+                    fs.writeFile(chunkPath,buffer, (err) => {
+                        if(err) {
+                            rejected(err);
+                        }
+                        const chunkData = {
+                            fileName: chunkName,
+                            extInf: extInf
+                        }
+                        resolve(chunkData);
+                    });
+                });
+            });
+
+            await this.updateM3UFile(lastChunkData);
+
+            streamWatchPageObj.lastBlockIndex++;
+        } catch(err) {
+            console.error("Unable handle stream data: \n" + err.toString());
+        }
+    }
+
+    async updateM3UFile(chunkData) {
+        const streamWatchPageObj = this;
+        const m3uPath = pathModule.join(streamWatchPageObj.currentStreamerPath, 'master.m3u8');
+        try {
+            await new Promise((resolve, rejected) => {                            
+                if(!fs.exists(m3uPath)) {
+                    const baseContent = `#EXTM3U
+                        #EXT-X-VERSION:3
+                        #EXT-X-TARGETDURATION:8
+                        #EXT-X-MEDIA-SEQUENCE:0
+                        #EXT-X-PLAYLIST-TYPE:EVENT`;
+                    fs.writeFile(m3uPath, baseContent, (err) => {
+                        if(err) {
+                            rejected(err);
+                        }
+                        resolve();
+                    }); 
+                } else {
+                    resolve();
+                }
+            });
+            await new Promise((resolve, rejected) => {
+                const chunkName = chunkData.fileName;
+                const extInf = chunkData.extInf;
+                try {
+                    fs.appendFileSync(m3uPath, chunkName);
+                    fs.appendFileSync(m3uPath, extInf);
+                    resolve();
+                } catch(err) {
+                    rejected(err);
+                }
+            });
+        } catch(err) {
+            console.error("Unable handle creation of m3ufile: \n" + err.toString());
+        }            
     }
 }
 
