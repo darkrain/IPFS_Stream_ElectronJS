7a238fca10c7c730ce2251cd3812f3c1d1167ac6
Vladimir <intfloatbool@gmail.com>
2019-09-26 12:20:59 +0300

    remove unecessary ' , '

--- src/helpers/ipfsLoaderHelper.js
+++ src/helpers/ipfsLoaderHelper.js
@@ -15,7 +15,7 @@ function initializeIPFS_Async() {
                 "/dns4/ws-star.discovery.libp2p.io/tcp/443/wss/p2p-websocket-star"
               ]
             }
-          },
+          }
       });
       
       ipfsInstance.setMaxListeners(0); //to avoid listeners limit
