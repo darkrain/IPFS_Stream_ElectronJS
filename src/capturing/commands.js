d838f52100cc779444168b6ce7b30c7ae37da6e2
Vladimir <intfloatbool@gmail.com>
2019-10-03 16:47:42 +0300

    Windows commands saved

--- src/capturing/commands.js
+++ src/capturing/commands.js
@@ -10,16 +10,36 @@ const KEYS = {
     CAM_KEY: 'CAM'
 }
 
+//DONT FORGET PUSH PATH TO PLAYLIST AFTER GET IT! In getters functions!
 function getSplittedLinuxCommand() {
-    //DONT FORGET PUSH PATH TO PLAYLIST AFTER GET IT!
+    
     const command = `-f v4l2 -video_size 640x480 -i ${KEYS.CAM_KEY} -f alsa -i default -c:v libx264 -preset ultrafast -c:a aac -f hls -pix_fmt yuv420p -hls_time 4 -hls_playlist_type event`;
     return command.split(" ");
+}
+
+function getWindowsDshowCommand() {
+    const commands = [
+        '-f' , 'dshow',
+        '-i', `${KEYS.CAM_KEY}`, 
+        '-profile:v', 'high422', //set profile to support 4:2:2 resolution
+        '-level', '3.0',
+        '-c:v', 'libx264',
+        '-crf','35', //crf is video qualiti from 1(best) to 51 (worst)
+        '-preset','veryfast',
+        '-c:a', 'aac', 
+        '-b:a', '128k', 
+        '-ac','2',
+        '-f', 'hls', 
+        '-pix_fmt', 'yuv420p', //to support 4:2:2 resoultion
+        '-hls_time', '4', 
+        '-hls_playlist_type', 'event', 
+    ]
 }
 
 //NOTE: this commands not including output path!!!
 const RecordCommands = {
     LINUX: getSplittedLinuxCommand(),
-    WINDOWS: [], //TODO complete Windows logic
+    WINDOWS: getWindowsDshowCommand(),
     MAC: [] //TODO complete MAC logic
 }
 
---
+++ src/capturing/ffmpegController.js
@@ -0,0 +1,11 @@
+const currentOs = require('../helpers/OSDetecting').getOs();
+const DeviceParserFactory = require('./deviceParserFactory');
+const RecorderFactory = require('./recorderFactory');
+
+class FFmpegController {
+    constructor() {
+        
+    }
+}
+
+module.exports = FFmpegController;
