835d7cf3a6543fee9f7a7e3374f9ad5f3929a4fa
Vladimir <intfloatbool@gmail.com>
2019-12-08 12:29:56 +0300

    GameEvent prototype

--- src/gameEvents/GameEvent.js
+++ src/gameEvents/GameEvent.js
@@ -1,5 +1,10 @@
 class GameEvent {
-
+    constructor(name, args) {
+        this.name = name;
+        //Args should contains any necessary data for event
+        //for example: if it is smartContract iteration- any values for web3
+        this.args = args;
+    }   
 }
 
 module.exports = GameEvent;
