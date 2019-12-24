da2492d2c57da976f08b893c368fdafdd6db219b
Vladimir <intfloatbool@gmail.com>
2019-12-24 11:03:32 +0300

    url generation

--- src/ipfsBinWork/IpfsBinRunner.js
+++ src/ipfsBinWork/IpfsBinRunner.js
@@ -7,10 +7,29 @@ class IpfsBinRunner {
             'daemon'
         ];
         this.ipfsProcess = spawn(appConfig.files.IPFS_BIN, opt);
-
         this.ipfsProcess.stdout.on(`data`, (msg) => {
-            console.log(`IPFS BIN: ${msg.toString()}`);
+            const msgStr = msg.toString();
+            console.log(`IPFS BIN: ${msgStr}`);
+
+            if(msg.includes('API')) {
+                this.parseApiServer(msgStr);
+            }
         });
+    }
+
+    parseApiServer(apiServerLine) {
+        const splittedBySpace = apiServerLine.split(' ');
+        const addr = splittedBySpace[4];
+        ///ip4/127.0.0.1/tcp/5001
+        //http://127.0.0.1:5001/
+        const splittedAddr = addr.split('/');
+        this.url = `http://${splittedAddr[2]}:${splittedAddr[4]}/`;
+
+        console.log(`ADDR OF IPFS API: ${this.url}`);
+    }
+
+    getUrl() {
+        return this.url;
     }
 
     getProcess() {
