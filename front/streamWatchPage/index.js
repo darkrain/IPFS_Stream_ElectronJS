f399fa63a8c2214843b427251ea3ab5bcbbc4cea
Vladimir <intfloatbool@gmail.com>
2019-12-24 12:43:57 +0300

    fix WIDTH issue

--- front/streamWatchPage/index.js
+++ front/streamWatchPage/index.js
@@ -56,7 +56,8 @@ $(document).ready(function() {
 ipc.on('stream-loaded', (event, args) => {
 	const playerRow = document.getElementById('playerRow');
 	const videoElem = document.createElement('video');
-	videoElem.width = "100%";
+	videoElem.width = 600;
+	videoElem.videoWidth 
 	videoElem.controls = true;
 	playerRow.append(videoElem);
 
